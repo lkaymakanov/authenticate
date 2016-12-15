51962c9a957bb13487ae225b7e1a23336bf84236
lkaymakanov <bat_lubo2001@abv.bg>
2016-12-15 10:48:05 +0200

    prepare token authentication!

--- src/token/TokenManager.java
+++ src/token/TokenManager.java
@@ -2,6 +2,7 @@ package token;
 
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * A class that manages tokens & data associated with token ids !!!
@@ -10,16 +11,45 @@ import java.util.Map;
 public class TokenManager {
 	private  static Map<String, ITokenData> map = new Hashtable<String, ITokenData>();
 	
-	public static void addTokenData(String tokenId, ITokenData data){
+	/***
+	 * Add token data to token map! The keys is the token id!
+	 * @param data token data
+	 */
+	public static void addTokenData(ITokenData data){
 		synchronized (TokenManager.class) {
-			map.put(tokenId, data);
+			map.put(data.getTokenId(), data);
 		}
 	}
 	
+	/**
+	 * Returns token data based on token id!
+	 * @param tokenId
+	 * @return Data associated with token id
+	 */
 	@SuppressWarnings("unchecked")
 	public static ITokenData getTokeData(String tokenId){
 		synchronized (TokenManager.class) {
 			return map.get(tokenId);
+		}
+	}
+	
+	/**
+	 * Remove token from token map by token id!
+	 * @param tokenId
+	 */
+	public static void removeToken(String tokenId){
+		synchronized (TokenManager.class) {
+			 map.remove(tokenId);
+		}
+	}
+	
+	/**
+	 * Returns set containing token ids!
+	 * @return token ids
+	 */
+	public static Set<String> getTokenids(){
+		synchronized (TokenManager.class) {
+			return map.keySet();
 		}
 	}
 }
