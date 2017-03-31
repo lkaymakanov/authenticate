a1e36e0a3211765bf05d1dd616a9a6592e762d86
lkaymakanov <bat_lubo2001@abv.bg>
2017-03-31 10:25:49 +0300

    change token constants

--- src/token/TokenConstants.java
+++ src/token/TokenConstants.java
@@ -10,8 +10,10 @@ public class TokenConstants {
 	public static final String TOKEN_ID_PARAM_NAME = "tokenid";
 	public static final String TOKEN_SESSION_ID_PARAM_NAME = "tokensessionid";
 	public static final String USER_ID_PARAM_NAME = "uid";
-	public static final String TOKEN_DATA_PATH = "/token/data";
-	public static final String TOKEN_VERIFY_PATH = "/token/verify";
+	public static final String TOKEN_DATA_PATH = "/data";
+	public static final String TOKEN_VERIFY_PATH = "/verify";
+	public static final String TOKEN_CONTROLLER_PATH = "/token";
+	
 	public static final String IVALID_TOKEN_ID = "IVALID_TOKEN_ID";
 
 	public enum AUTHENTICATION_TYPE{
